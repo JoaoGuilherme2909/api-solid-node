d8c344f9db31cbe226a9c8b199be961d41a65db0
Joao guilherme dos Santos <joaoguilhermesantos2909@gmail.com>
2024-11-17 14:29:07 -0400

    Users repository created, password hashing created and create user route created

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
-  {
-    rules: {
-      quotes: ["error", "single"],
-      semi: ["error", "never"], 
-      "no-unused-vars": ["error"], 
-      indent: ["error", 2], 
-    },
-  },
   ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
